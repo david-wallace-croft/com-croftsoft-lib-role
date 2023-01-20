603f61e88dcd152db41bd76de931897cd000232d
David Wallace Croft <david@CroftSoft.com>
2023-01-19 20:07:57 -0600

    version 0.1.4-SNAPSHOT

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+// =============================================================================
+//! - com-croftsoft-lib-role
+//!
+//! # Metadata
+//! - Copyright: &copy; 2023 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Version: 2023-01-19
+//! - Since: 2023-01-18
+//!
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
 pub trait Painter {
   fn paint(&self);
 }
