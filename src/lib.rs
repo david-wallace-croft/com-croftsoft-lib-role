6424815d0bf706e499e1dd1e9e0f8821070cc9e5
David Wallace Croft <david@CroftSoft.com>
2023-01-21 09:43:41 -0600

    version 0.1.4

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,16 @@
 //! # Metadata
 //! - Copyright: &copy; 2023 [`CroftSoft Inc`]
 //! - Author: [`David Wallace Croft`]
-//! - Version: 2023-01-19
+//! - Version: 2023-01-21
 //! - Since: 2023-01-18
 //!
 //! [`CroftSoft Inc`]: https://www.croftsoft.com/
 //! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
 // =============================================================================
+
+pub trait Initializer {
+  fn initialize(&mut self);
+}
 
 pub trait Painter {
   fn paint(&self);
