2deaa67bf606b92f51d764bcd12b24efff7d8b21
David Wallace Croft <david@CroftSoft.com>
2023-05-13 09:45:02 -0500

    preparer

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! - Copyright: &copy; 2023 [`CroftSoft Inc`]
 //! - Author: [`David Wallace Croft`]
 //! - Created: 2023-01-18
-//! - Updated: 2023-03-03
+//! - Updated: 2023-05-13
 //!
 //! [`CroftSoft Inc`]: https://www.croftsoft.com/
 //! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
@@ -17,6 +17,10 @@ pub trait Initializer {
 
 pub trait Painter {
   fn paint(&mut self);
+}
+
+pub trait Preparer {
+  fn prepare(&mut self);
 }
 
 pub trait Updater {
